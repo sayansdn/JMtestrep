d817993c6bbcc97baae0565ad23bf747b7f24373
sayansdn <sayansdn@gmail.com>
2023-11-09 19:35:32 +0530

    test23

--- main.tf
+++ main.tf
@@ -1,9 +1,10 @@
 
 terraform {
   backend "azurerm" {
-    storage_account_name = "teststg"
-    container_name       = "testcnt"
-    #key                  = "varvmName"
+    resource_group_name = "TestRG"
+    storage_account_name = "Teststg"
+    container_name       = "Testcnt"
+    key                  = "terra.tfstate"
     #access_key            = var.access_key
     #access_key = "q3kRLfKlcbOF6Y+bMCzkEHR1BNK2cbDrFdLsb73H06ceUqdcutdBG1l4Tkh6z39e61ess3+a+7wK+AStAUJilw=="
 
@@ -19,24 +20,17 @@ terraform {
 
 provider "azurerm" {
   features {}
-  environment     = "public"
-  subscription_id = "79170484-10cc-48f5-99a2-f4bfe2ad50e1"
+  #environment     = "public"
+  #subscription_id = "79170484-10cc-48f5-99a2-f4bfe2ad50e1"
   # client_id       = var.spnID
   # client_secret   = var.spnPass
   # tenant_id       = var.tenantID
 }
 
-# # Bootstrapping Script
-# data "template_file" "tf-script" {
-#   template = file("setup.ps1")
-# }
-
-#Data Modules
-
 
 #Create a resource group
 
 resource "azurerm_resource_group" "rgname" {
-  name     = "testRG"
-  location = "West us"
+  name     = "NewTestRG"
+  location = "West Europe"
 }
