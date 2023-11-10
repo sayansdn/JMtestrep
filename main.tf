1126c181d2214f2c20c8797e863917488eca4208
sayansdn <sayansdn@gmail.com>
2023-11-10 22:30:13 +0530

    update

--- main.tf
+++ main.tf
@@ -21,13 +21,13 @@ terraform {
 
 provider "azurerm" {
   features {}
-  #use_msi = true
+  use_msi = true
   environment     = "production"
   subscription_id = "79170484-10cc-48f5-99a2-f4bfe2ad50e1"
   client_id       = "dc156045-af12-4b19-ac45-9535124c50ac"
   # client_secret   = var.spnPass
   tenant_id       = "baaae90c-2f56-4229-bdc1-7a0a8192b487"
-  ARM_MSI_ENDPOINT = false
+  #ARM_MSI_ENDPOINT = false
   
 }
 
