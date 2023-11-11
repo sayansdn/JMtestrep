e4ba74944de6310372d84b199ed488ecd93983d2
sayansdn <sayansdn@gmail.com>
2023-11-11 10:43:28 +0530

    lnx

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 
 terraform {
-  /*backend "azurerm" {
+  backend "azurerm" {
     resource_group_name = "TestRG"
     storage_account_name = "teststgxyxx"
     container_name       = "testcnt"
@@ -9,7 +9,7 @@ terraform {
     #access_key            = var.access_key
     #access_key = "q3kRLfKlcbOF6Y+bMCzkEHR1BNK2cbDrFdLsb73H06ceUqdcutdBG1l4Tkh6z39e61ess3+a+7wK+AStAUJilw=="
 
-  }*/
+  }
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
@@ -27,7 +27,7 @@ provider "azurerm" {
   #client_id       = "dc156045-af12-4b19-ac45-9535124c50ac"
   #client_secret   = var.spnPass
   #tenant_id       = "baaae90c-2f56-4229-bdc1-7a0a8192b487"
-  #msi_endpoint = false
+  msi_endpoint = true
   
 }
 
